45dee24da71a3fb7f3f15f4606647cb244d02893
Kate Shlykova <e2.shlykova@itransition.com>
2022-06-26 13:07:50 +0300

    feat: add using Stream API

--- src/http_server/index.ts
+++ src/http_server/index.ts
@@ -2,16 +2,16 @@ import * as fs from 'fs';
 import * as path from 'path';
 import * as http from 'http';
 
-export const httpServer = http.createServer(function (req, res) {
-    const __dirname = path.resolve(path.dirname(''));
-    const file_path = __dirname + (req.url === '/' ? '/front/index.html' : '/front' + req.url);
-    fs.readFile(file_path, function (err, data) {
-        if (err) {
-            res.writeHead(404);
-            res.end(JSON.stringify(err));
-            return;
-        }
-        res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'})
-        res.end(data);
-    });
+export const httpServer = http.createServer((req, res) => {
+    try {
+        const __dirname = path.resolve(path.dirname(''));
+        const file_path = __dirname + (req.url === '/' ? '/front/index.html' : '/front' + req.url);
+        const readableStream = fs.createReadStream(file_path);
+
+        res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
+        readableStream.pipe(res);
+    } catch(error) {
+        res.writeHead(404);
+        res.end(JSON.stringify(error));
+    }
 });
--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,33 @@
 import { httpServer } from './http_server/index';
 import robot from 'robotjs';
-import { WebSocketServer } from 'ws';
+import { WebSocketServer, createWebSocketStream } from 'ws';
 import * as shapes from './shapes';
 import * as image from './image';
 
 const HTTP_PORT = 3000;
+const WS_PORT = 8080;
 
 console.log(`Start static http server on the ${HTTP_PORT} port!`);
 httpServer.listen(HTTP_PORT);
 
 const wss = new WebSocketServer({
-  port: 8081,
+  port: WS_PORT,
 });
 
 wss.on('connection', (ws) => {
-  ws.on('message', async (data) => {
-    const args = data.toString().split(' ');
+  console.log(`Start web socket on the ${WS_PORT} port!`);
+
+  const webSocketStream = createWebSocketStream(ws, {encoding: 'utf8', decodeStrings: false});
+  let data = '';
+  
+  webSocketStream.on('readable', async() => {
+    let chunk;
+
+    while ((chunk = webSocketStream.read()) !== null) {
+      data += chunk;
+    }
+
+    const args = data.split(' ');
     const command = args[0];
     const arg1 = Number(args[1]);
     const arg2 = Number(args[2]);
@@ -25,58 +37,59 @@ wss.on('connection', (ws) => {
         const { x, y } = robot.getMousePos();
         robot.moveMouse(x, y - arg1);
         const { x:newX, y:newY } = robot.getMousePos();
-        ws.send(`mouse_up__(currX:${newX},currY:${newY})\0`);
+        webSocketStream.write(`mouse_up__(currX:${newX},currY:${newY})\0`);
         break;
       }
       case 'mouse_down': {
         const { x, y } = robot.getMousePos();
         robot.moveMouse(x, y + arg1);
         const { x:newX, y:newY } = robot.getMousePos();
-        ws.send(`mouse_down__(currX:${newX},currY:${newY})\0`);
+        webSocketStream.write(`mouse_down__(currX:${newX},currY:${newY})\0`);
         break;
       }
       case 'mouse_left': {
         const { x, y } = robot.getMousePos();
         robot.moveMouse(x - arg1, y);
         const { x:newX, y:newY } = robot.getMousePos();
-        ws.send(`mouse_left__(currX:${newX},currY:${newY})\0`);
+        webSocketStream.write(`mouse_left__(currX:${newX},currY:${newY})\0`);
         break;
       }
       case 'mouse_right': {
         const { x, y } = robot.getMousePos();
         robot.moveMouse(x + arg1, y);
         const { x:newX, y:newY } = robot.getMousePos();
-        ws.send(`mouse_right__(currX:${newX},currY:${newY})\0`);
+        webSocketStream.write(`mouse_right__(currX:${newX},currY:${newY})\0`);
         break;
       }
       case 'mouse_position': {
         const { x, y } = robot.getMousePos();
-        ws.send(`mouse_position ${x},${y}\0`);
+        webSocketStream.write(`mouse_position ${x},${y}\0`);
         break;
       }
       case 'draw_circle': {
         shapes.drawCircle(arg1);
-        ws.send(`draw_circle\0`);
+        webSocketStream.write(`draw_circle\0`);
         break;
       }
       case 'draw_square': {
         shapes.drawSquare(arg1);
-        ws.send(`draw_square\0`);
+        webSocketStream.write(`draw_square\0`);
         break;
       }
       case 'draw_rectangle': {
         shapes.drawRectangle(arg1, arg2);
-        ws.send(`draw_rectangle\0`);
+        webSocketStream.write(`draw_rectangle\0`);
         break;
       }
       case 'prnt_scrn': {
         const printScreen = await image.printScreen();
         const base64 = await printScreen.getBase64Async(printScreen.getMIME());
         const imageString = base64.substring(22);
-        ws.send(`prnt_scrn ${imageString}\0`);
+        webSocketStream.write(`prnt_scrn ${imageString}\0`);
         break;
       }
     }
+    data = '';
   });
 });
 
